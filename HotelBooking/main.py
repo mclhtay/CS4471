1d8499cea70fb3c3f2d5196011e605cca40892cc
Carter Jin <carter.jin6487@gmail.com>
2022-10-07 21:50:44 -0400

    add sqlite support and update diagrams to reflect model changes

--- HotelBooking/main.py
+++ HotelBooking/main.py
@@ -1,3 +1,11 @@
-# Main entry of the app
+from sqlmodel import create_engine, Session, select
 
-print("Hello world")
+from Models.room import Room
+
+
+def test_db_connection():
+    # TODO: Remove this test block
+    Room().get_available_rooms()
+
+
+test_db_connection()
