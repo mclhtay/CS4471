b85f70a0eda5fe077d5f449bb75a0619d0ad2b1d
Carter Jin <carter.jin6487@gmail.com>
2022-10-08 21:14:04 -0400

    update class diagram with new controller properties

--- HotelBooking/main.py
+++ HotelBooking/main.py
@@ -1,5 +1,4 @@
 from HotelBooking.Controllers.room_controller import RoomController
 
 if __name__ == "__main__":
-    room_controller = RoomController()
-    room_controller.check_in_room("single19", 1)
+    print("Hello")
