2138e8c2aea959cc9d87296c5c5b268b34684d4a
vzhang23 <115050680+vzhang23@users.noreply.github.com>
2022-10-15 16:27:05 -0400

    Add instructions to run on windows machine
    Add instructions to run on windows machine

--- HotelBooking/run.sh
+++ HotelBooking/run.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-pip3 install -r requirements.txt --user
+
+#if this shell script is not working, navigate you self to the home directory of the HotelBooker project and type 
+#command 'python3 -m HotelBooking'
+
+
+#add --user at the end of the command if you are using windows computer
+pip3 install -r requirements.txt
 
 pre-commit install
 
 cd ..
 
-py -m HotelBooking
+python3 -m HotelBooking.main
