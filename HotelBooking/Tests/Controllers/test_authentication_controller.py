4e6fedd59b1b68c8670f81d67a570e9eaca5cc12
Tala Buwadi <tbuwadi@gmail.com>
2022-11-16 17:13:52 -0500

    added tests for authentication controller and customer model

--- HotelBooking/Tests/Controllers/test_authentication_controller.py
+++ HotelBooking/Tests/Controllers/test_authentication_controller.py
@@ -27,3 +27,25 @@ class TestAuthenticationController:
 
         mock_authenticate_customer.assert_called_once_with(
             mock_id, mock_pw)
+    
+    @patch.object(Customer, "customer_exists")
+    def test_customer_exists(self, mock_customer_exists):
+        mock_id = "test"
+
+        controller = AuthenticationController()
+        controller.customer_exists(mock_id)
+
+        mock_customer_exists.assert_called_once_with(mock_id)
+
+    @patch.object(Customer, "create_customer")
+    def test_create_customer(self, mock_create_customer):
+        customer_id="username1"
+        customer_password="mypass"
+        customer_name="Jane Doe"
+        customer_address= "123 Western Rd"
+        customer_cell_number=1234567890
+        customer_credit_card_number = 134212
+
+        controller = AuthenticationController()
+        controller.create_customer(customer_id, customer_password, customer_name, customer_address, customer_cell_number, customer_credit_card_number)
+        mock_create_customer.assert_called_once_with(customer_id, customer_password, customer_name, customer_address, customer_cell_number, customer_credit_card_number)
