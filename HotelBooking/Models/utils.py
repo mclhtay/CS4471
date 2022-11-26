a094c67a5f021f9422f321a0835ba935eb5915fe
Carter Jin <carter.jin6487@gmail.com>
2022-11-25 19:08:02 -0500

    add comment for db address logic

--- HotelBooking/Models/utils.py
+++ HotelBooking/Models/utils.py
@@ -4,7 +4,12 @@ from sqlmodel import create_engine
 
 
 def get_database_address():
+    """
+    Database addresses are different depending on whether the project
+    is being executed as a script file or as a frozen system executable.
+    """
     if getattr(sys, 'frozen', False):
+        # is frozen as executable
         db_address = os.path.join(
             os.path.dirname(sys.executable), 'DB/Hotel.db')
     else:
